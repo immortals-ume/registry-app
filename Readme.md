2a18b6973c8f77992598d173303f585a5c62287e
immortals-ume <kaish127799@outlook.com>
2023-06-18 21:28:38 +0530

    registry app using eureka server

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Registry App 
+
+For monitoring and registering the services running on cloud 
---
+++ src/test/java/com/immortals/registryapp/RegistryAppApplicationTests.java
@@ -0,0 +1,14 @@
+package com.immortals.registryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
