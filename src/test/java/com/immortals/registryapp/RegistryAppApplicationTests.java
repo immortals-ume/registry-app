bc4f42fe2aed860943cb5680cc51ce65a81b283e
immortals-ume <kaish127799@outlook.com>
2023-07-15 12:15:49 +0530

    Implmented Registry Service

--- src/test/java/com/immortals/registryapp/RegistryAppApplicationTests.java
+++ src/test/java/com/immortals/registryapp/RegistryAppApplicationTests.java
@@ -1,14 +1,21 @@
 package com.immortals.registryapp;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @SpringBootTest
 class RegistryAppApplicationTests {
 
-	@Test
-	void contextLoads() {
+    @Value("${server.port}")
+    private int serverPort;
 
-	}
+    @Test
+    void contextLoads() {
+        assertEquals(8761, serverPort, "Server is not running on the expected port");
+    }
 
 }
--- src/test/java/com/immortals/registryapp/it/EurekaServerIntegrationTests.java
+++
@@ -1,2 +0,0 @@
-package com.immortals.registryapp.it;public class EurekaServerIntegrationTests {
-}
